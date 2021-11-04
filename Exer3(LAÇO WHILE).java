7af8f5cdf83dc1c6ff9a30e45b41fd869792376d
Mauricio Ferreira <mauricio52f@gmail.com>
2021-11-04 16:22:57 -0300

    Primeiro commit

---
+++ Exer3(LAÇO WHILE).java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Exercicio3 {
+
+	public static void main(String[]args) {
+		Scanner input = new Scanner(System.in);
+		
+		int n; 
+		long fatorial =1;
+		
+		System.out.println("Entre com o numero");
+		n = input.nextInt();
+		
+		if(n<0) {
+			System.out.println("Não coloque números negativos");
+			
+		}else if (n<=1) {
+			System.out.printf("%d! = %d"+n,fatorial);
+		}else {
+			for( int counter = n; counter >= 2; counter-- ) {
+				fatorial = fatorial*counter;
+			}
+			System.out.printf("%d! = %d"+n, fatorial);
+		}
+	}
+}
+		
