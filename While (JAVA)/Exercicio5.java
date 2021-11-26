bd1dd801d7ef003b8940426245d0d15c186b1342
Mauricio57lk <92936639+Mauricio57lk@users.noreply.github.com>
2021-11-25 21:08:06 -0300

    Adicionando arquivos Java

---
+++ While (JAVA)/Exercicio5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class questao4 {
+	public static void main(String args[]) {
+		Scanner in = new Scanner (System.in);
+		
+
+					int i=3, b=0;
+					do{
+						b = b+i;
+						i+=3;
+					System.out.println(i);
+					}while (i<=300);
+					System.out.println(b);
+					
+				}
+
+}
